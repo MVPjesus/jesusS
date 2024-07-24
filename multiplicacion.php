083cdca1286650ae5e7ee38507f5606e5305e182
MVPjesus <gitjesuseliamruffo@gmail.com>
2024-07-23 23:58:21 -0400

    mitare

---
+++ multiplicacion.php
@@ -0,0 +1,12 @@
+<?php
+
+// Definimos las notas en un arreglo
+$notas = [8.5, 7.2, 6.9];
+
+// Calculamos el promedio utilizando la función array_sum para sumar los elementos del arreglo
+$promedio = array_sum($notas) / count($notas);
+
+// Mostramos el resultado
+echo "El promedio de las notas " . implode(", ", $notas) . " es: " . number_format($promedio, 2);
+
+?>
---
+++ sumade3.php
@@ -0,0 +1,18 @@
+<?php
+
+// Función para calcular el promedio de 3 notas
+function calcularPromedio($nota1, $nota2, $nota3) {
+    $promedio = ($nota1 + $nota2 + $nota3) / 3;
+    return $promedio;
+}
+
+// Definimos las notas
+$nota1 = 8.5;
+$nota2 = 7.2;
+$nota3 = 6.9;
+
+// Llamamos a la función y mostramos el resultado
+$resultado = calcularPromedio($nota1, $nota2, $nota3);
+echo "El promedio de las notas $nota1, $nota2 y $nota3 es: " . number_format($resultado, 2);
+
+?>
