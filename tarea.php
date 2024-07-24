0d955a564608d11f25a4db3fdd96cd0045351a74
MVPjesus <gitjesuseliamruffo@gmail.com>
2024-07-23 23:35:19 -0400

    aprendiendo

---
+++ tarea.php
@@ -0,0 +1,15 @@
+<?php
+
+// Definimos las notas
+$nota1 = 8.5;
+$nota2 = 7.2;
+$nota3 = 6.9;
+
+// Calculamos el promedio
+$promedio = ($nota1 + $nota2 + $nota3) / 3;
+
+// Mostramos el resultado
+echo "El promedio de las notas $nota1, $nota2 y $nota3 es: " . number_format($promedio, 2);
+
+?>
+
